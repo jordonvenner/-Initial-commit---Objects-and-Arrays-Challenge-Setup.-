57616b809dca9b067f2aded93edc9d9618e45a01
Jordon Venner <jordonvenner@usf.edu>
2025-01-30 12:58:21 -0500

    Task 1 - Product Price Management.

---
+++ cc_3.js
@@ -0,0 +1,10 @@
+//Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system.
+
+//Step 1
+let prices = [13, 16.4, 23.3, 12.34, 11.43];
+//Step 2
+prices.push(54.12);
+//Step 3
+prices.shift(13);
+//Step 4
+console.log("Updated Prices:", prices)
